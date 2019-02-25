f626859bf4171f8f8e94f62300d98f52eb9343cf
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-02-25 12:51:38 +0100

    Updated too new code structure

--- sv_post_latest.php
+++ sv_post_latest.php
@@ -1,57 +1,67 @@
 <?php
-	namespace sv_100;
-	
-	/**
-	 * @author			Matthias Reuter
-	 * @package			sv_100
-	 * @copyright		2017 Matthias Reuter
-	 * @link			https://straightvisions.com
-	 * @since			1.0
-	 * @license			See license.txt or https://straightvisions.com
-	 */
-	class sv_post_latest extends init{
-		static $scripts_loaded						= false;
+namespace sv_100;
 
-		public function __construct($path,$url){
-			$this->path								= $path;
-			$this->url								= $url;
-			$this->name								= get_class($this);
-		}
-		public function init() {
-			add_shortcode($this->get_module_name(), array($this, 'shortcode'));
-			add_action('widgets_init', array($this, 'sidebars'));
-		}
+/**
+ * @version         1.00
+ * @author			straightvisions GmbH
+ * @package			sv_100
+ * @copyright		2017 straightvisions GmbH
+ * @link			https://straightvisions.com
+ * @since			1.0
+ * @license			See license.txt or https://straightvisions.com
+ */
 
-		public function shortcode($settings, $content=''){
-			$settings								= shortcode_atts(
-				array(
-					'inline'						=> true,
-					'title'							=> '+++ UPDATES +++',
-					'id'							=> false,
-					'limit'							=> 3,
-					'order'							=> 'date',
-				),
-				$settings,
-				$this->get_module_name()
-			);
-			$this->module_enqueue_scripts($settings['inline']);
-			ob_start();
-			include($this->get_file_path('lib/tpl/frontend.php'));
-			$output									= ob_get_contents();
-			ob_end_clean();
-			
-			return $output;
-		}
-		public function sidebars(){
-			register_sidebar(array(
+class sv_post_latest extends init {
+	public function __construct() {
+
+	}
+
+	public function init() {
+		// Module Info
+		$this->set_module_title( 'SV Post Latest' );
+		$this->set_module_desc( __( 'This module gives the ability to display the latest posts via the "[sv_post_latest]" shortcode.', $this->get_module_name() ) );
+
+		// Shortcodes
+		add_shortcode($this->get_module_name(), array($this, 'shortcode'));
+
+		// Action Hooks
+		add_action('widgets_init', array($this, 'sidebars'));
+	}
+
+	public function shortcode( $settings, $content = '' ) {
+		// Load Styles
+		static::$scripts->create( $this )
+		                ->set_source( $this->get_file_url( 'lib/css/frontend.css' ), $this->get_file_path( 'lib/css/frontend.css' ) );
+
+		$settings								= shortcode_atts(
+			array(
+				'inline'						=> true,
+				'title'							=> '+++ UPDATES +++',
+				'id'							=> false,
+				'limit'							=> 3,
+				'order'							=> 'date',
+			),
+			$settings,
+			$this->get_module_name()
+		);
+
+		ob_start();
+		include( $this->get_file_path( 'lib/tpl/frontend.php' ) );
+		$output									= ob_get_contents();
+		ob_end_clean();
+
+		return $output;
+	}
+
+	public function sidebars() {
+		register_sidebar(array(
 			'name'									=> __('Latest Posts Sidebar', 'sv_100'),
-			'id'									=> $this->name,
+			'id'									=> $this->get_module_name(),
 			'description'							=> __( 'Widgets in this area will be shown in with Latest Posts List.', 'sv_100' ),
 			'before_widget'							=> '<li id="%1$s" class="widget d-flex align-items-center %2$s">',
 			'after_widget'							=> '</li>',
 			'before_title'							=> '<h2 class="'.$this->name.'">',
 			'after_title'							=> '</h2>',
-			));
-		}
+		));
 	}
-?>
+}
